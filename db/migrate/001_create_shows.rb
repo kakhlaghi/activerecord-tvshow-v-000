b31bbf1301c78c98a1639599508efe424a5c0d1c
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-25 22:59:47 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
-  create_table :shows do |t|
+  create_table shows do |t|
     t.integer :name
     t.integer :network
     t.integer :select_day
